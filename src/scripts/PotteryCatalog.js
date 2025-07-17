f5c528db274dd1db9b414d35aa0aa0e206056132
charlesmerriman <zacmerr@gmail.com>
2025-07-17 09:45:00 -0500

    Created function for displaying uncracked pottery

--- src/scripts/PotteryCatalog.js
+++ src/scripts/PotteryCatalog.js
@@ -1,7 +1,7 @@
-displayPottery = []
+const displayPottery = []
 
 export const toSellOrNotToSell = (pottery) => {
-    if (pottery.cracked = false) {
+    if (pottery.cracked === false) {
         if (pottery.weight >= 6) {
             pottery.price = 40
         }
@@ -12,4 +12,9 @@ export const toSellOrNotToSell = (pottery) => {
     }
 }
 
-export const usePottery = displayPottery.map()
+function usePottery() {
+    const potteryForSale = structuredClone(displayPottery);
+    return potteryForSale
+}
+
+export { usePottery };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js"
 import { firepottery } from "./Kiln.js"
+import { toSellOrNotToSell } from "./PotteryCatalog.js"
+import { usePottery } from "./PotteryCatalog.js"
 
 // Make 5 pieces of pottery at the wheel
 const mug = makePottery("mug", 5, 10)
@@ -15,23 +17,27 @@ console.log(vase)
 console.log(bowl)
 console.log(pitcher)
 
+// Fire each piece of pottery in the kiln
 const firedMug = firepottery(mug, 2300)
 const firedPlate = firepottery(plate, 2000)
 const firedVase = firepottery(vase, 1900)
 const firedBowl = firepottery(bowl, 2100)
 const firedPitcher = firepottery(pitcher, 2200)
 
-
 console.log(firedMug)
 console.log(firedPlate)
 console.log(firedVase)
 console.log(firedBowl)
 console.log(firedPitcher)
-// Fire each piece of pottery in the kiln
-
 
 // Determine which ones should be sold, and their price
+toSellOrNotToSell(firedMug)
+toSellOrNotToSell(firedPlate)
+toSellOrNotToSell(firedVase)
+toSellOrNotToSell(firedBowl)
+toSellOrNotToSell(firedPitcher)
 
+console.log(usePottery())
 
 // Invoke the component function that renders the HTML list
 
